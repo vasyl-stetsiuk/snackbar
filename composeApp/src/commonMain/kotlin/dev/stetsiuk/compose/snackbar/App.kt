ddfdeb6276c509f6cd1b6af246e4bf4097eb1803
vasylstetsiuk <vasil.stecyuk@kevychsolutions.com>
2025-12-27 08:20:56 +0200

    Fix

--- composeApp/src/commonMain/kotlin/dev/stetsiuk/compose/snackbar/App.kt
+++ composeApp/src/commonMain/kotlin/dev/stetsiuk/compose/snackbar/App.kt
@@ -16,8 +16,8 @@ import androidx.compose.ui.Modifier
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
-import snackbar.composeapp.generated.resources.Res
-import snackbar.composeapp.generated.resources.compose_multiplatform
+import snackbarjetpackcompose.composeapp.generated.resources.Res
+import snackbarjetpackcompose.composeapp.generated.resources.compose_multiplatform
 
 @Composable
 @Preview
